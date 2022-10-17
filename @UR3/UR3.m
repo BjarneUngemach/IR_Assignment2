0f3f388299a7b4c1cebab09d6b6c456609962f43
BenniLeb <benjamin.lebherz@student.uts.edu.au>
2022-10-17 14:34:19 +1100

    upload correct files

---
+++ @UR3/UR3.m
@@ -0,0 +1,65 @@
+classdef UR3 < handle
+    
+    properties
+        %> Robot model
+        ur3;
+    end
+    
+    methods%% Class for UR3 robot simulation
+        function self = UR3()
+                        
+            self.GetUR3Robot();
+            self.PlotAndColourUR3();
+
+            drawnow
+        end
+
+        %% GetUR3Robot
+        % Given a name (optional), create and return a UR3 robot model
+        function GetUR3Robot(self)
+            name = 'UR3';
+            L1 = Link('d',0.1519,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            L2 = Link('d',0,'a',-0.24365,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+            L3 = Link('d',0,'a',-0.21325,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
+            L4 = Link('d',0.11235,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            L5 = Link('d',0.08535,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            L6 = Link('d',0.0819,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+            
+            self.ur3 = SerialLink([L1 L2 L3 L4 L5 L6],'name',name);
+            
+        end
+
+        %% PlotAndColourRobot
+        % Given a robot index, add the glyphs (vertices and faces) and
+        % colour them in if data is available 
+        function PlotAndColourUR3(self)
+            for linkIndex = 0:self.ur3.n
+                [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(['ur3link_',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>                
+                self.ur3.faces{linkIndex + 1} = faceData;
+                self.ur3.points{linkIndex + 1} = vertexData;
+            end
+
+            % Display robot
+            self.ur3.plot3d(zeros(1,self.ur3.n),'noarrow','workspace',self.workspace);
+            if isempty(findobj(get(gca,'Children'),'Type','Light'))
+                camlight
+            end  
+            self.ur3.delay = 0;
+
+            % Try to correctly colour the arm (if colours are in ply file data)
+            for linkIndex = 0:self.ur3.n
+                handles = findobj('Tag', self.ur3.name);
+                h = get(handles,'UserData');
+                try 
+                    h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+                                                                  , plyData{linkIndex+1}.vertex.green ...
+                                                                  , plyData{linkIndex+1}.vertex.blue]/255;
+                    h.link(linkIndex+1).Children.FaceColor = 'interp';
+                catch ME_1
+                    disp(ME_1);
+                    continue;
+                end
+            end
+        end        
+    end
+end
